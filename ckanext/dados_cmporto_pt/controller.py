ce7cdcbc245a8e51f41471149d8b518bab3aacf1
Filipe Correia <filipe.correia@paradigmaxis.pt>
2015-09-18 14:37:22 +0100

    Removed the 'px-' prefix from template files created specifically for CMP.

--- ckanext/dados_cmporto_pt/controller.py
+++ ckanext/dados_cmporto_pt/controller.py
@@ -13,17 +13,17 @@ class AdminController(admin.AdminController):
 
 class StaticPagesController(base.BaseController):
     def linked_data(self):
-        return base.render('home/cmp/px-linked-data.html')
+        return base.render('home/cmp/linked-data.html')
 
     def terms_of_use(self):
-        return base.render('home/cmp/px-terms-of-use.html')
+        return base.render('home/cmp/terms-of-use.html')
 
     def privacy_policy(self):
-        return base.render('home/cmp/px-privacy-policy.html')
+        return base.render('home/cmp/privacy-policy.html')
 
     def moderation_policy(self):
-        return base.render('home/cmp/px-moderation-policy.html')
+        return base.render('home/cmp/moderation-policy.html')
 
     def list_license(self):
-        return base.render('home/cmp/px-license-list.html', extra_vars = {'licenses': Package.get_license_register().items()})
+        return base.render('home/cmp/license-list.html', extra_vars = {'licenses': Package.get_license_register().items()})
 
