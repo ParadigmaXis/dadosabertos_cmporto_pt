8d037773c9471710d78c19daaefc4089dee6cef3
José Vilaça <jose.vilaca@paradigmaxis.pt>
2015-10-20 16:30:33 +0100

    Implemented automatic publication into GeoServer of SHP resources that are uploaded.

--- ckanext/dados_cmporto_pt/plugin.py
+++ ckanext/dados_cmporto_pt/plugin.py
@@ -7,6 +7,7 @@ from ckan.common import g
 import json
 import utils
 
+from geoserver_integration import upload_shapefile_resource
 
 import logging
 log = logging.getLogger(__name__)
@@ -117,11 +118,42 @@ def sorted_guia_extras(package_extras):
     return output
 
 class ShapefilePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IResourceController, inherit=True)
     plugins.implements(plugins.IResourceView, inherit=True)
     plugins.implements(plugins.IConfigurable, inherit=True)
 
+    def get_resource_storage_path(self, resource_id):
+        return '{0}/resources/{1}/{2}/{3}'.format(self.storage_path, resource_id[0:3], resource_id[3:6], resource_id[6:])
+
+    def before_create(self, context, resource):
+        pass
+    def after_create(self, context, resource):
+        try:
+            if self.can_view_resource(resource):
+                resource_id = resource['id']
+                upload_shapefile_resource(self.geoserver_url, self.geoserver_user, self.geoserver_password, resource_id, self.get_resource_storage_path(resource_id))
+        except Exception as e:
+            log.exception(e)
+            raise e
+    def before_update(self, context, current, resource):
+        pass
+    def after_update(self, context, resource):
+        if self.can_view_resource(resource):
+            resource_id = resource['id']
+            upload_shapefile_resource(self.geoserver_url, self.geoserver_user, self.geoserver_password, resource_id, self.get_resource_storage_path(resource_id))
+    def before_delete(self, context, resource, resources):
+        pass
+    def after_delete(self, context, resources):
+        pass
+    def before_show(self, resource):
+        pass
+
     def configure(self, config):
         self.gapi_key = config.get('ckanext.geoview.gapi_key', None)
+        self.storage_path = config.get('ckan.storage_path', None)
+        self.geoserver_url = config.get('dadosabertos.geoserver.url', None)
+        self.geoserver_user = config.get('dadosabertos.geoserver.user', None)
+        self.geoserver_password = config.get('dadosabertos.geoserver.password', None)
 
     def info(self):
         return {'name': 'shapefile_view',
@@ -133,6 +165,9 @@ class ShapefilePlugin(plugins.SingletonPlugin):
 
     def can_view(self, data_dict):
         resource = data_dict.get('resource', None)
+        return self.can_view_resource(resource)
+
+    def can_view_resource(self, resource):
         if resource:
             url_type = resource.get('url_type', '')
             format_lower = resource.get('format', '').lower()
