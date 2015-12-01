242cb56645bfed8aa95a0a3beaed97a8fdd2fe12
Pedro Abreu <pedro.abreu@paradigmaxis.pt>
2015-12-01 14:53:59 +0000

    Corrected cron script

--- docker/ckan/ckan-search-index-rebuild.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /etc/envvars
-source $(dirname "$0")/utils.bash
-
-# Wait for dependencies
-wait_for_lock /root/start.lock
-
-"$APP_HOME"/bin/paster --plugin=ckan search-index rebuild -r -c "$CKAN_CONFIG/$CONFIG_FILE"
